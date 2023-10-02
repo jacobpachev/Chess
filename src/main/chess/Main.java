a54c5033bc46e1777486165ec75d754884cf05be
jacobpachev <jacobpachev@gmail.com>
2023-10-02 13:46:41 -0600

    Working on castle functionality, can castle properly, but tries to castle in/through check

--- src/main/chess/Main.java
+++ src/main/chess/Main.java
@@ -4,15 +4,14 @@ public class Main {
     public static void main(String[] args) throws InvalidMoveException {
         ChessBoard board = new MyBoard();
         GameImpl game = new GameImpl();
-        board.addPiece(new MyPosition(7,3), new Pawn(ChessGame.TeamColor.BLACK));
-        board.addPiece(new MyPosition(5,2), new Pawn(ChessGame.TeamColor.WHITE));
-//        board.addPiece(new MyPosition(5,6), new Pawn(ChessGame.TeamColor.WHITE));
-        game.setTeamTurn(ChessGame.TeamColor.BLACK);
+        board.addPiece(new MyPosition(8,5), new King(ChessGame.TeamColor.BLACK));
+        board.addPiece(new MyPosition(8,1), new Rook(ChessGame.TeamColor.BLACK));
+        board.addPiece(new MyPosition(8,8), new Rook(ChessGame.TeamColor.BLACK));
+        board.addPiece(new MyPosition(4,6), new Rook(ChessGame.TeamColor.WHITE));game.setTeamTurn(ChessGame.TeamColor.BLACK);
         game.setBoard(board);
-        game.makeMove(new MyChessMove(new MyPosition(7,3), new MyPosition(5,3)));
-        System.out.println(game.validMoves(new MyPosition(5,2)));
-        game.makeMove(new MyChessMove(new MyPosition(5,2), new MyPosition(6,3)));
-        game.setTeamTurn(ChessGame.TeamColor.WHITE);
+        System.out.println(game.validMoves(new MyPosition(8,5)));
+        System.out.println(board);
+        game.makeMove(new MyChessMove(new MyPosition(8,5), new MyPosition(8,7)));
         System.out.println(board);
 
 
