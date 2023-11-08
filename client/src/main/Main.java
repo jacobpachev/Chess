c58aa9468873d39e474071195fca6fd390e38eba
jacobpachev <jacobpachev@gmail.com>
2023-11-08 14:32:35 -0700

    Black board view now goes from h-a(not a-h)

--- client/src/main/Main.java
+++ client/src/main/Main.java
@@ -18,14 +18,16 @@ public class Main {
     static void renderBoard(ChessBoard board, String color) {
         var range = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
         var rangeReversed = new ArrayList<>(Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1));
+        var letters = "    h  g  f  e  d  c  b  a    ";
         if (color.equals("white")) {
             range = new ArrayList<>(Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1));
             rangeReversed = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+            letters = "    a  b  c  d  e  f  g  h    ";
         }
         System.out.print(SET_TEXT_BOLD);
         System.out.print(SET_BG_COLOR_LIGHT_GREY);
         System.out.print(SET_TEXT_COLOR_BLACK);
-        System.out.print("    a  b  c  d  e  f  g  h    ");
+        System.out.print(letters);
         System.out.print(RESET_BG_COLOR);
         System.out.println(RESET_TEXT_COLOR);
         for(var j : range) {
@@ -39,7 +41,7 @@ public class Main {
                 var pieceStr = " ";
                 if(piece != null) {
                     var pieceColor = (piece.getTeamColor() == ChessGame.TeamColor.WHITE) ? SET_TEXT_COLOR_RED : SET_TEXT_COLOR_BLUE;
-                    pieceStr = piece.toString();
+                    pieceStr = piece.toString().toUpperCase();
                     System.out.print(pieceColor);
                 }
                 System.out.print(" "+pieceStr+" ");
@@ -54,7 +56,7 @@ public class Main {
         }
         System.out.print(SET_BG_COLOR_LIGHT_GREY);
         System.out.print(SET_TEXT_COLOR_BLACK);
-        System.out.print("    a  b  c  d  e  f  g  h    ");
+        System.out.print(letters);
         System.out.print(RESET_BG_COLOR);
         System.out.println(RESET_TEXT_COLOR);
     }
